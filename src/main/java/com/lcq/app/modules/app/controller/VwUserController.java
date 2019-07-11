294fa38bdafe3a82a9a67545821f485daec843b7
gongsi <linchuangqiong66@163.com>
2019-07-11 16:54:15 +0800

    添加app用户登录接口

--- src/main/java/com/lcq/app/modules/app/controller/VwUserController.java
+++ src/main/java/com/lcq/app/modules/app/controller/VwUserController.java
@@ -1,9 +1,12 @@
 package com.lcq.app.modules.app.controller;
 
 import com.lcq.app.modules.app.service.VwUserService;
+import com.lcq.app.modules.system.controller.vo.ResultVO;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +24,17 @@ public class VwUserController{
     @Autowired
     private VwUserService vwUserService;
 
+    /**
+     * 用户登录
+     * @param username
+     * @param password
+     * @return
+     */
+    @ApiOperation("用户登录")
+    @PostMapping("login")
+    public ResultVO login(String username, String password) {
+        ResultVO resultVO = new ResultVO();
 
+        return resultVO;
+    }
 }
